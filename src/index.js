53dc761b8643165bd02cd994caa071b7c1aa83f4
fumitaka fujisaki <87471293+gumifu@users.noreply.github.com>
2021-12-08 06:13:16 +0900

    let,const

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import "./styles.css";
+//変数宣言について const let
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "ajijfi";
+// console.log(val2);
+
+// val2 = "kokorefk";
+// console.log(val2);
+
+// let val2="jiaji";//できない！！！
+
+const val3 = "kojifeji";
+console.log(val3);
+
+// const val3 ="kofakofekkfaopaf";
+//constで定義したオブジェｌクトであれば、プロパティーは上書き可能！！！
+const val4 = {
+  name: "fujisaki",
+  age: "55歳"
+};
+// console.log(val4);
+
+val4.name = "jakisaki";
+val4.address = "hiroshima"; //追加可能
+console.log(val4);
